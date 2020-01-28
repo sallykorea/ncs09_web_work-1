968ac6c0a20375e296122c7226793bb520425f5c
kimhyeonkyung <khk5087@naver.com>
2020-01-28 13:16:18 +0900

    Spring+Maven project 세팅 및 테스트

---
+++ Spring01_Java/src/test/example3/CarImpl.java
@@ -0,0 +1,12 @@
+package test.example3;
+
+import test.mypac.Car;
+
+public class CarImpl implements Car{
+
+	@Override
+	public void dirve() {
+		System.out.println("운전을 해요!");
+	}
+	
+}
---
+++ Spring02_Hello/src/main/java/com/gura/spring02/HomeController.java
@@ -0,0 +1,28 @@
+package com.gura.spring02;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/*
+ *  @Controller 어노테이션
+ *  - 해당 클래스가 Spring MVC 프로젝트에서 Controller 가 될 수 있도록 한다.
+ *  - componeent scan을 통해서 spring bean container 에서 관리가 되는 bean이 되어야 동작을 한다.
+ */
+@Controller
+public class HomeController {
+	
+	// /home.do 요청이 왔을때 요청을 처리하게 하는 @RequestMapping 어노테이션
+	@RequestMapping("/home")
+	public String home() {
+		/*
+		 *  "home" 을 리턴해주면
+		 *	
+		 *	 "/WEB-INF/views/"+"home"+".jsp" 와 같은 문자열이 만들어지도
+		 *	
+		 *	/WEB-INF/views/home.jsp 페이지로 forward 이동 되어서 응답된다.
+		 *
+		 */
+		return "home";
+	}
+	
+}
