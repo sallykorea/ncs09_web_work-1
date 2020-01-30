f81383711351eb1a3b745ff84ecb55af0a6c8fe2
kimhyeonkyung <khk5087@naver.com>
2020-01-30 10:07:48 +0900

    Spring04 필기 추가, @After 표현법 사용 테스트

--- Spring04_AOP_Java/src/test/aspect/PenAspect.java
+++ Spring04_AOP_Java/src/test/aspect/PenAspect.java
@@ -1,5 +1,6 @@
 package test.aspect;
 
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -13,8 +14,14 @@ public class PenAspect {
 	 *  리턴 type 은 void, 메소드 명은 write로 시작, 전달되는 인자가 없는
 	 *  메소드 수행 이전(@Before)에 할 작업
 	 */
-	@Before("execution(void write*())")
+	@Before("execution(void write*())") //@Before("어떤 곳에 적용할지 명시")
 	public void prepare() {
 		System.out.println("Pen을 준비해요!");
 	}
+	
+	@After("execution(void write*())")
+	public void after() {
+		System.out.println("Pen을 마무리해요!");
+	}
+	
 }
--- Spring04_AOP_Java/src/test/main/Mainclass.java
+++ Spring04_AOP_Java/src/test/main/Mainclass.java
@@ -14,8 +14,11 @@ public class Mainclass {
 		
 		System.out.println("---------");
 		
-		ApplicationContext context=new ClassPathXmlApplicationContext("test/main/init.xml");
-		WritingUtil util2=context.getBean(WritingUtil.class);
+		
+		//메인메소드가 실행 되는 시점에 객체가 생성되고, 메인 메소드가 종료되면 객체는 사라진다. 
+		//스프링에서는 서버가 종료되기 전까지 객체가 생성되되고, 스프링에 의해 계속해서 객체가 관리된다.
+		ApplicationContext context=new ClassPathXmlApplicationContext("test/main/init.xml");//어디에 있는 문서를 해석해서 객체를 만들지 지정하는 객체.
+		WritingUtil util2=context.getBean(WritingUtil.class);//spring bean context로  부터 'WritingUtil.class'type을 찾아서 해당 객체의 참조값을 리턴해준다.
 		util2.write1();
 		util2.write2();
 		util2.write3();
