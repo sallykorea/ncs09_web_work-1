4c378a72b447eaef081330ccf33aa8a5f2bdaa2d
kimhyeonkyung <khk5087@naver.com>
2020-02-03 15:04:26 +0900

    Spring05 회원가입 기능 추가

--- Spring05_Final/src/main/java/com/gura/spring05/users/controller/UsersController.java
+++ Spring05_Final/src/main/java/com/gura/spring05/users/controller/UsersController.java
@@ -4,10 +4,14 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.gura.spring05.users.dto.UsersDto;
 import com.gura.spring05.users.service.UsersService;
 
 @Controller
@@ -30,5 +34,12 @@ public class UsersController {
 	public Map<String, Object> checkid(@RequestParam String inputId){
 		Map<String, Object> map=service.isExistId(inputId);
 		return map;
+	}
+	
+	@RequestMapping(value = "/users/signup", method = RequestMethod.POST)
+	public ModelAndView signup(@ModelAttribute("dto") UsersDto dto, ModelAndView mView) {
+		service.addUser(dto);
+		mView.setViewName("users/insert");
+		return mView;
 	}
 }
