29f35f9a4d5522cef9d8a3b23ee5b8ff5cf60ec7
kimhyeonkyung <khk5087@naver.com>
2020-02-05 12:27:18 +0900

    spring05 file 목록 요청 구현

--- Spring05_Final/src/main/java/com/gura/spring05/file/controller/FileController.java
+++ Spring05_Final/src/main/java/com/gura/spring05/file/controller/FileController.java
@@ -1,5 +1,7 @@
 package com.gura.spring05.file.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,5 +14,12 @@ public class FileController {
 	@Autowired
 	private FileService service;
 	
+	@RequestMapping("/file/list")
+	public ModelAndView list(ModelAndView mView, HttpServletRequest request) {
+		//파일 목록과 페이징 처리에 필요한 값들을 request에 담아주는 서비스 메소드 호출하기
+		service.list(request);
+		mView.setViewName("file/list");
+		return mView;
+	}
 	
 }
--- Spring05_Final/src/main/java/com/gura/spring05/file/dao/FileDaoImpl.java
+++ Spring05_Final/src/main/java/com/gura/spring05/file/dao/FileDaoImpl.java
@@ -1,11 +1,28 @@
 package com.gura.spring05.file.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import com.gura.spring05.file.dto.FileDto;
 
 @Repository
 public class FileDaoImpl implements FileDao{
 	@Autowired
 	private SqlSession session;
+
+	@Override
+	public int getCount() {
+		return session.selectOne("file.getCount");
+	}
+
+	@Override
+	public List<FileDto> getList(FileDto dto) {
+		List<FileDto> list=session.selectList("file.getList", dto);
+		return list;
+	}
+	
+	
 }
