6e9863ddfb9626037e5d8ebc6ffb9fa7af6d95a8
kimhyeonkyung <khk5087@naver.com>
2020-02-06 12:45:18 +0900

    spring05 게시판(cafe) list 출력 기능 구현 완료

--- Spring05_Final/src/main/java/com/gura/spring05/cafe/controller/CafeController.java
+++ Spring05_Final/src/main/java/com/gura/spring05/cafe/controller/CafeController.java
@@ -1,5 +1,23 @@
 package com.gura.spring05.cafe.controller;
 
-public class CafeController {
+import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.gura.spring05.cafe.service.CafeService;
+
+@Controller
+public class CafeController {
+	@Autowired
+	private CafeService service;
+	
+	@RequestMapping("/cafe/list")
+	private ModelAndView list(ModelAndView mView, HttpServletRequest request) {
+		service.list(request);
+		mView.setViewName("cafe/list");
+		return mView;
+	}
 }
--- Spring05_Final/src/main/java/com/gura/spring05/cafe/dao/CafeDaoImpl.java
+++ Spring05_Final/src/main/java/com/gura/spring05/cafe/dao/CafeDaoImpl.java
@@ -1,5 +1,27 @@
 package com.gura.spring05.cafe.dao;
 
-public class CafeDaoImpl implements CafeDao{
+import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.gura.spring05.cafe.dto.CafeDto;
+
+@Repository
+public class CafeDaoImpl implements CafeDao{
+	@Autowired
+	private SqlSession session;
+
+	@Override
+	public int getCount(CafeDto dto) {
+		int count=session.selectOne("cafe.getCount", dto);
+		return count; //리턴한다.
+	}
+
+	@Override
+	public List<CafeDto> getList(CafeDto dto) {
+		List<CafeDto> list=session.selectList("cafe.getList", dto);
+		return list;
+	}
 }
